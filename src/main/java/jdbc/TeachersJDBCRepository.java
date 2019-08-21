867db3e7e484148bfe9187fbe760021273238fda
Krzysztof Atlasik <krzysztof.atlasik@protonmail.com>
2019-08-21 21:34:54 +0200

    Updated solutions. Added exercise with procedure.

--- src/main/java/jdbc/TeachersJDBCRepository.java
+++ src/main/java/jdbc/TeachersJDBCRepository.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import javax.sql.DataSource;
+
 import jdbc.model.Teacher;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,66 +17,82 @@ import org.springframework.jdbc.support.KeyHolder;
 
 public class TeachersJDBCRepository {
 
-  private final JdbcTemplate jdbcTemplate;
-  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-  public TeachersJDBCRepository(DataSource dataSource) {
-    jdbcTemplate = new JdbcTemplate(dataSource);
-    namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-  }
+    public TeachersJDBCRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
 
-  public Optional<Teacher> findTeacherById(Long id) {
-    return jdbcTemplate.query(
-        "SELECT id, first_name, last_name FROM teachers WHERE id = ?",
-        new Object[] {id},
-        new TeacherResultSetExtractor());
-  }
+    /**
+     * Zaimplementuj `findTeacherById` wzracający obiekt `Teacher` po `id.
+     */
+    public Optional<Teacher> findTeacherById(Long id) {
+        return jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM teachers WHERE id = ?",
+                new Object[]{id},
+                new TeacherResultSetExtractor());
+    }
 
-  public List<Teacher> findAllTeachers() {
-    return jdbcTemplate.query(
-        "SELECT id, first_name, last_name FROM teachers", new TeacherRowMapper());
-  }
+    /**
+     * Zaimplementuj `findAllTeacher` zwracający wszystkich nauczycieli.
+     */
+    public List<Teacher> findAllTeachers() {
+        return jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM teachers", new TeacherRowMapper());
+    }
 
-  public List<Teacher> findTeachersWhere(String query) {
-    return namedParameterJdbcTemplate.query(
-        "SELECT teachers.id, first_name, last_name FROM teachers "
-            + "JOIN school_classes ON school_classes.teacher_id = teachers.id "
-            + "WHERE lower(last_name) LIKE lower(:query) "
-            + "OR lower(first_name) LIKE lower(:query) "
-            + "OR lower(school_classes.name) LIKE lower(:query) ",
-        new MapSqlParameterSource("query", "%" + query + "%"),
-        new TeacherRowMapper());
-  }
+    /**
+     * Zaimplementuj metodę `findTeachersWhere` wyszukującą nauczycieli po imienu lub nazwisku lub nazwie prowadzonego przez niego przedmiotu.
+     * Wyszukiwanie powinno być *case-insensitive*, czyli wielkość znaków nie powinna mieć znaczenia.
+     */
+    public List<Teacher> findTeachersWhere(String query) {
+        return namedParameterJdbcTemplate.query(
+                "SELECT teachers.id, first_name, last_name FROM teachers "
+                        + "JOIN school_classes ON school_classes.teacher_id = teachers.id "
+                        + "WHERE lower(last_name) LIKE lower(:query) "
+                        + "OR lower(first_name) LIKE lower(:query) "
+                        + "OR lower(school_classes.name) LIKE lower(:query) ",
+                new MapSqlParameterSource("query", "%" + query + "%"),
+                new TeacherRowMapper());
+    }
 
-  public Teacher createTeacher(Teacher teacher) {
-    KeyHolder keyHolder = new GeneratedKeyHolder();
+    /**
+     * Zaimplementuj metodę `createTeacher`, pozwalającą na dodanie nowego nauczyciela.
+     */
+    public Teacher createTeacher(Teacher teacher) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
 
-    namedParameterJdbcTemplate.update(
-        "INSERT INTO teachers(first_name, last_name) VALUES(:firstName, :lastName)",
-        new MapSqlParameterSource("firstName", teacher.getFirstName())
-            .addValue("lastName", teacher.getLastName()),
-        keyHolder);
+        namedParameterJdbcTemplate.update(
+                "INSERT INTO teachers(first_name, last_name) VALUES(:firstName, :lastName)",
+                new MapSqlParameterSource("firstName", teacher.getFirstName())
+                        .addValue("lastName", teacher.getLastName()),
+                keyHolder);
 
-    return new Teacher(
-        keyHolder.getKey().longValue(), teacher.getFirstName(), teacher.getLastName());
-  }
+        return new Teacher(
+                keyHolder.getKey().longValue(), teacher.getFirstName(), teacher.getLastName());
+    }
 
-  public int batchCreateTeachers(List<Teacher> teachers) {
+    /**
+     * Zaimplementuj metodę `batchCreateTeachers`, pozwalającą na zbiorcze dodanie nowych nauczycieli.
+     */
+    public int batchCreateTeachers(List<Teacher> teachers) {
 
-    int[] modified =
-        jdbcTemplate.batchUpdate(
-            "INSERT INTO teachers(first_name, last_name) VALUES(?, ?)",
-            new BatchPreparedStatementSetter() {
-              public void setValues(PreparedStatement ps, int i) throws SQLException {
-                ps.setString(1, teachers.get(i).getFirstName());
-                ps.setString(2, teachers.get(i).getLastName());
-              }
+        int[] modified =
+                jdbcTemplate.batchUpdate(
+                        "INSERT INTO teachers(first_name, last_name) VALUES(?, ?)",
+                        new BatchPreparedStatementSetter() {
+                            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                                ps.setString(1, teachers.get(i).getFirstName());
+                                ps.setString(2, teachers.get(i).getLastName());
+                            }
 
-              public int getBatchSize() {
-                return teachers.size();
-              }
-            });
+                            public int getBatchSize() {
+                                return teachers.size();
+                            }
+                        });
 
-    return Arrays.stream(modified).sum();
-  }
+        return Arrays.stream(modified).sum();
+    }
 }
